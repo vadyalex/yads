573e64885792182d6cfbe8620bea08a6c42d2cfe
Vladyslav Aleksakhin <vadyalex@gmail.com>
2017-03-16 23:18:33 +0100

    Update Travis CI configuration and add build budge into README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # yads
 Dynamic DNS micro service connected to Yandex DNS API
+
+[![Build Status](https://travis-ci.org/vadyalex/yads.svg?branch=master)](https://travis-ci.org/vadyalex/yads)
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vadyalex/yads "1.0.0-SNAPSHOT"
+(defproject vadyalex/yads "1.0.0-SNAPSHOT"
 
   :description "Dynamic DNS micro service connected to Yandex DNS API"
 
